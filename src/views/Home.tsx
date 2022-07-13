7c2b6c60f02531f4b9d1565ee3de914ee0bae7cf
A.Colas <anthonincolaspro@gmail.com>
2022-07-14 01:46:05 +0200

    fix de certaine chose et enfin ajout du css

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Box, ChakraProps, Container } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import { LandingVideo } from '../Components/Landingvideo/LandingVideo';
 import { components } from '../api/typings/api';
 import { fetcher } from '../api/fetcher';
+import styles from './GlobalStyle.module.css';
 
 function Home() {
   const [landing, setLanding] = useState([] as components['schemas']['LandingResponse']);
@@ -21,45 +22,30 @@ function Home() {
   useEffect(() => {
     result();
   }, []);
+  //https://api-gratflix.onrender.com/api/movies?filters[category]=7&populate=category
 
   return (
     <Box position='relative'>
       <LandingVideo landing={landing} />
-      <Box {...(swiperContainerSettings as ChakraProps)}>
-        <Container {...(GalerieSettings as ChakraProps)}>
+      <Box className={styles.swiperContainer} as='section'>
+        <Container className={styles.galerie} as='article' style={{ maxWidth: 'container.xxl' }}>
           {/* <Galerie list={1} listTitle={'Tendances actuelles'}/> */}
         </Container>
-        <Container {...(GalerieSettings as ChakraProps)}>
+        <Container className={styles.galerie} as='article'>
           {/* <Galerie list={6} listTitle={'Sélectionnés pour vous'}/> */}
         </Container>
-        <Container {...(GalerieSettings as ChakraProps)} mb={50}>
+        <Container className={styles.galerie} as='article' mb={50}>
           {/* <Galerie list={7} listTitle={'oe'}/> */}
         </Container>
-        <Container {...(GalerieSettings as ChakraProps)} mb={50}>
+        <Container className={styles.galerie} as='article' mb={50}>
           {/* <Galerie list={2} listTitle={'oeoe'}/> */}
         </Container>
-        <Container {...(GalerieSettings as ChakraProps)} mb={50}>
+        <Container className={styles.galerie} as='article' mb={50}>
           {/* <Galerie list={8} listTitle={'test'}/> */}
         </Container>
       </Box>
     </Box>
   );
 }
-
-const GalerieSettings = {
-  maxW: 'container.xxl',
-  as: 'article',
-  mb: 5,
-  mt: 5,
-};
-
-const swiperContainerSettings = {
-  boxShadow: '1px 0px 69px 140px rgb(20, 20, 20)',
-  zIndex: 100,
-  ml: 55,
-  mr: 55,
-  position: 'relative',
-  as: 'section',
-};
 
 export default Home;
